b3e0d4e172f261d67cbf88abcd0355fe8b814049
Harsh <panchalharshcd@gmail.com>
2026-04-07 15:17:20 +0530

    feat - protected Dashboard page

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export default auth((req) => {
     const isLoggedIn = !!req.auth ;
     const isAuthPage = req.nextUrl.pathname.startsWith("/login") || req.nextUrl.pathname.startsWith("/signup");
-    const isProtectedPage = req.nextUrl.pathname.startsWith('/editor') || req.nextUrl.pathname.startsWith('/user')
+    const isProtectedPage = req.nextUrl.pathname.startsWith('/editor') || req.nextUrl.pathname.startsWith('/user') || req.nextUrl.pathname.startsWith('/dashboard')
 
     if(isProtectedPage && !isLoggedIn){
         return NextResponse.redirect(new URL('/login',req.url))
